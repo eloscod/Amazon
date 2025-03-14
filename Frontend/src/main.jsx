4aa22aa787c4b587c31790fe6158c49ea68b0c3a
Osama Mohammed Hussen <136145403+eloscod@users.noreply.github.com>
2025-03-14 08:23:23 +0300

    analytic not working

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,14 +5,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { DataProvider } from "./Components/DataProvider/DataProvider.jsx";
 import { initialState, reducer } from "./Utility/reducer.js";
-import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <DataProvider reducer={reducer} initialState={initialState}>
         <App />
-        <Analytics />
       </DataProvider>
     </BrowserRouter>
   </StrictMode>
